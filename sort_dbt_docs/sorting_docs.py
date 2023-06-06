700784d0873d14b2dfe4d6350dbd58198efd37d7
Leopold Gabelmann <leopold.gabelmann@waschbaer.de>
2023-06-06 14:35:48 +0200

    Set up folder structure to follow the python package guidelines.

--- sort_dbt_docs/sorting_docs.py
+++ sort_dbt_docs/sorting_docs.py
@@ -1,11 +1,11 @@
-''' Sorting script for the docs markdowns.
+"""Sorting script for the docs markdowns.
 
 We sort our markdown docs alphabetically. When adding or deleting docs blocks it can easily happen
-that the alphabetical order is messed up. Running this script is sorting the docs alphabetically 
+that the alphabetical order is messed up. Running this script is sorting the docs alphabetically
 into a new markdown file and replacing the original file with the sorted one.
 
 In order for the script to return the desired outcome it is crucial that the format for each doc is
-as follows, including the line breaks: 
+as follows, including the line breaks:
 
 {% docs name_of_column %}
 
@@ -13,36 +13,39 @@ Description of column.
 
 {% enddocs %}
 
-'''
-
+"""
 import os
 import re
 import shutil
 
 # Get the list of markdown files in the folder
-folder_path = 'macros\docs'  # Set the folder path where the markdown files are located
-markdown_files = [file for file in os.listdir(folder_path) if file.startswith('doc_') and file.endswith('.md')]
+folder_path = r"macros\docs"  # Set the folder path where the markdown files are located
+markdown_files = [
+    file
+    for file in os.listdir(folder_path)
+    if file.startswith("doc_") and file.endswith(".md")
+]
 
 # Sort and replace each markdown file
 for file_name in markdown_files:
     file_path = os.path.join(folder_path, file_name)
 
-    with open(file_path, 'r') as f:
+    with open(file_path) as f:
         markdown_text = f.read()
 
-    pattern = r'{% docs (.+?) %}\n(.*?)\n{% enddocs %}'
+    pattern = r"{% docs (.+?) %}\n(.*?)\n{% enddocs %}"
     docs_blocks = re.findall(pattern, markdown_text, flags=re.DOTALL)
 
     sorted_docs_blocks = sorted(docs_blocks, key=lambda x: x[0].lower())
 
-    sorted_markdown = ''
+    sorted_markdown = ""
     for block in sorted_docs_blocks:
-        sorted_markdown += f'{{% docs {block[0]} %}}\n{block[1]}\n{{% enddocs %}}\n\n\n'
+        sorted_markdown += f"{{% docs {block[0]} %}}\n{block[1]}\n{{% enddocs %}}\n\n\n"
 
     # Create a temporary file path for the sorted markdown
-    temp_file_path = file_path + '.temp'
+    temp_file_path = file_path + ".temp"
 
-    with open(temp_file_path, 'w') as f:
+    with open(temp_file_path, "w") as f:
         f.write(sorted_markdown)
 
     # Replace the original file with the sorted file
